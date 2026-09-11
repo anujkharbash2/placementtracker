f462ba5742adf5ab544f9789e8a825f57d3c47b5
anujkharbash2 <anujkharbash2@email.com>
2026-09-11 21:34:33 +0530

    basic setup and routing scaffold

---
+++ client/src/Login.jsx
@@ -0,0 +1,12 @@
+function Login(){
+    return (
+        <div>
+            <h1>Aap Kon?</h1>
+            <p> Jldi hi aapki phachan hogi...</p>
+            abhi k liye aap <a href="/student">Student</a> hai ya <a href="/recruiter">Recruiter</a> ya <a href="/admin">Admin</a>
+
+        </div>
+    )
+}
+
+export default Login;
---
+++ client/src/dashboards/Student.jsx
@@ -0,0 +1,10 @@
+function student(){
+    return ( 
+        <div>
+            <h1>WELCOME, Bachha!</h1>
+            <p>Jld hi aapki placement lagwai jayegi!!</p>
+        </div>
+    )
+}
+
+export default student;
